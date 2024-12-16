a1a70466c0533ecdb7e84bf3b7a8fd4f69c30232
Tr-frzlata <Zlatrush@gmail.com>
2024-12-16 20:38:50 +0300

    Создание главной функции

--- develop.py
+++ develop.py
@@ -1,1 +1,12 @@
+import pandas as pd
+from openpyxl import load_workbook
 
+def update_files(file_paths):
+    last_id = None
+    print("Начинаем обновление файлов...")
+
+# Пути к файлам
+file_paths = ['file1.xlsx', 'file2.xlsx', 'file3.xlsx']
+
+# Вызов функции
+update_files(file_paths)
